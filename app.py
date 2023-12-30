6e70d3c3338bfeede716327474af51ae7b64e351
Yashvardhan Baid <codechaser2002@gmail.com>
2023-12-30 13:16:16 +0530

    moved html code to templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from src import get_chaughadiya, get_muhurat
 
 app = Flask(__name__)
@@ -30,77 +30,21 @@ def api_documentation():
     """
     Returns the documentation for the API.
     """
-    return """
-    <h1>Chaughadiya API</h1>
-    <p>API to get chaughadiya and muhurat.</p>
-    <table>
-        <thead>
-            <tr>
-                <th>Endpoint</th>
-                <th>Method</th>
-                <th>Parameters</th>
-                <th>Description</th>
-                <th>Example</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>/api/get-chaughadiya</td>
-                <td>GET</td>
-                <td>
-                    <ul>
-                        <li>date: string (format: YYYY-MM-DD)</li>
-                        <li>latitude: float</li>
-                        <li>longitude: float</li>
-                    </ul>
-                </td>
-                <td>Returns the chaughadiya for the given date.</td>
-                <td><a href="/api/get-chaughadiya?date=2021-01-01&latitude=23.0225&longitude=72.5714">/api/get-chaughadiya?date=2021-01-01&latitude=23.0225&longitude=72.5714</a></td>
-            </tr>
-            <tr>
-                <td>/api/get-muhurat</td>
-                <td>GET</td>
-                <td>
-                    <ul>
-                        <li>timestamp: string (format: YYYY-MM-DD HH:MM:SS)</li>
-                        <li>latitude: float</li>
-                        <li>longitude: float</li>
-                    </ul>
-                </td>
-                <td>Returns the muhurat for the given timestamp with the chaughadiya of that day.</td>
-                <td><a href="/api/get-muhurat?timestamp=2021-01-01 12:00:00&latitude=23.0225&longitude=72.5714">/api/get-muhurat?timestamp=2021-01-01 12:00:00&latitude=23.0225&longitude=72.5714</a></td>
-            </tr>
-        </tbody>
-    </table>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-            padding: 10px;
-        }
-    </style>
-    <p><b>Note:</b> Please provide date and time in UTC timezone.</p>
-    """
+    return render_template('./static/api-documentation.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
     """
     Returns the 404 page.
     """
-    return """
-    <h1>404</h1>
-    <p>Looks like you are lost. Go back to <a href="/">home</a>.</p>
-    """, 404
+    return render_template('./static/page-not-found.html'), 404
 
 @app.route('/')
 def home():
     """
     Returns the home page.
     """
-    return """
-    <h1>Chaughadiya API</h1>
-    <p>Go to <a href="/api/docs">documentation</a>.</p>
-    """
+    return render_template('./static/home.html')
 
 
 if __name__ == '__main__':
